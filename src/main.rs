fc2dd1e5e834a28485f0b06fae69afd44e1352ff
Jared <jareddzwick@gmail.com>
2023-11-16 23:59:01 -0500

    use gpt vision

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,19 @@
 // it should have some reference of what i am looking at and working on
 // eventually it should do more than just gpt, maybe embed stuff and RAG
 
+use base64::encode;
 use clap::Parser;
-use std::error::Error;
 use serde::{Deserialize, Serialize};
-use tokio::runtime::Runtime;
-use log::LevelFilter;
-use log::info;
-use std::collections::HashMap;
+use std::error::Error;
+use std::fs::{read_dir, remove_file, File};
+use std::io::Read;
 use std::process::Command;
-use rusty_tesseract::{Args,Image};
+use tokio::runtime::Runtime;
 
 #[derive(Parser)]
 struct Cli {
     command: String,
-    param: String
+    param: String,
 }
 
 #[derive(Serialize)]
@@ -30,60 +29,100 @@ struct ChatInput {
 #[derive(Serialize, Deserialize)]
 struct Message {
     role: String,
+    content: Vec<ContentItem>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct MessageOut {
+    role: String,
     content: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
 struct ChatOutput {
-    choices: Vec<Choice>,
+    choices: Vec<ChoiceOut>,
 }
 
-#[derive(Deserialize)]
-struct Choice {
-    message: Message,
+#[derive(Serialize, Deserialize)]
+struct ChoiceOut {
+    message: MessageOut,
 }
 
 struct WindowInfo {
     id: String,
 }
 
+#[derive(Serialize, Deserialize)]
+struct ContentItem {
+    #[serde(rename = "type")]
+    content_type: String,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    text: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    image_url: Option<ImageUrl>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct ImageUrl {
+    url: String,
+}
+
 fn main() {
     let args = Cli::parse();
-
-    simple_logging::log_to_file("test.log", LevelFilter::Info).expect("error setting up logger");
 
     let rt = Runtime::new().unwrap();
     match args.command.as_str() {
         "gpt" => {
-            let future = make_gpt_api_call(args.param);
+            let future = make_gpt_api_call(args.param, None);
             rt.block_on(future).expect("Error making api call");
         }
         "win" => {
-            let ocrd_screenshots = wmctrl("-lx");
-            let prompt = format!("These are OCR strings from the windows currently open on my computer {} ",ocrd_screenshots.join(" "));
-            let future = make_gpt_api_call(format!("{} {}", prompt, args.param));
-            println!("{}", prompt);
+            let b64_screenshots = wmctrl("-lx");
+            let future = make_gpt_api_call(format!("{}", args.param), Some(b64_screenshots));
             rt.block_on(future).expect("Error making api call");
         }
-        _ =>{ println!("unknown command") }
+        _ => {
+            println!("unknown command")
+        }
     }
+    cleanup_tmp_dir();
 }
 
-async fn make_gpt_api_call(msg: String) -> Result<(), Box<dyn Error>> {
-
+async fn make_gpt_api_call(
+    msg: String,
+    b64_screenshots: Option<Vec<String>>,
+) -> Result<(), Box<dyn Error>> {
     let token = std::env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY not set");
+    let mut content_items = vec![ContentItem {
+        content_type: "text".to_string(),
+        text: Some(msg),
+        image_url: None,
+    }];
 
+    if let Some(screenshots) = b64_screenshots {
+        for screenshot in screenshots {
+            let image_url_item = ContentItem {
+                content_type: "image_url".to_string(),
+                text: None,
+                image_url: Some(ImageUrl {
+                    url: format!("data:image/jpeg;base64,{}", screenshot),
+                }),
+            };
+            content_items.push(image_url_item);
+        }
+    }
     let chat_input = ChatInput {
-        model: "gpt-4".to_string(),
+        model: "gpt-4-vision-preview".to_string(),
         messages: vec![Message {
             role: "user".to_string(),
-            content: msg.clone(),
+            content: content_items,
         }],
         temperature: 0.7,
     };
 
     let api_url = "https://api.openai.com/v1/chat/completions";
-
+    // println!("\n\nF\n {}\n\n", json_string);
     let client = reqwest::Client::new();
     let response = client
         .post(api_url)
@@ -96,9 +135,9 @@ async fn make_gpt_api_call(msg: String) -> Result<(), Box<dyn Error>> {
     let text = response.text().await?;
     let chat_output: ChatOutput = serde_json::from_str(&text).expect(&text);
 
-    println!("{}", chat_output.choices[0].message.content);
+    let response_string = serde_json::to_string_pretty(&chat_output).unwrap();
+    println!("{}", response_string);
 
-    info!("msg: {}, \n response: {}", msg.clone(), chat_output.choices[0].message.content);
     Ok(())
 }
 
@@ -121,9 +160,7 @@ fn wmctrl(args: &str) -> Vec<String> {
 
         let id = parts[0].to_string();
 
-        let window = WindowInfo {
-            id,
-        };
+        let window = WindowInfo { id };
 
         windows.push(window);
     }
@@ -135,36 +172,45 @@ fn wmctrl(args: &str) -> Vec<String> {
     for window in &windows {
         Command::new("sh")
             .arg("-c")
-            .arg(format!("xwd -id {} -out ./tmp/{}.xwd", window.id, window.id))
+            .arg(format!(
+                "xwd -id {} -out ./tmp/{}.xwd",
+                window.id, window.id
+            ))
             .output()
             .expect("xwd failed");
 
-       let convert_output = Command::new("sh")
+        let convert_output = Command::new("sh")
             .arg("-c")
-            .arg(format!("convert ./tmp/{}.xwd -alpha off -negate  ./tmp/{}.png", window.id, window.id)) //-negate
+            .arg(format!(
+                "convert ./tmp/{}.xwd -alpha off -negate  ./tmp/{}.png",
+                window.id, window.id
+            )) //-negate
             .output()
             .expect("image magick failed");
-        if convert_output.status.success(){
-            let ocr = read_image(window.id.clone());
-            if ocr.len() > 20 {
-                outputs.push(ocr);
-            }
+        if convert_output.status.success() {
+            let b64string = base_64_image(window.id.clone());
+            outputs.push(b64string);
         }
     }
     return outputs;
 }
 
-fn read_image(w_id: String) -> String {
-    let img = Image::from_path(format!("./tmp/{}.png", w_id)).expect("unable to read image");
-    let my_args = Args {
-        lang: "eng".to_string(),
-        config_variables: HashMap::from([(
-            "tessedit_char_whitelist".into(),"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ".into(),
-            )]),
-        dpi: Some(300),
-        psm: Some(6),
-        oem: Some(3)
-    };
-    let output = rusty_tesseract::image_to_string(&img, &my_args).unwrap();
-    return output;
+fn base_64_image(w_id: String) -> String {
+    let mut file = File::open(format!("./tmp/{}.png", w_id)).expect("Unable to read image");
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)
+        .expect("Error loading image into buffer");
+    return encode(&buffer);
+}
+
+fn cleanup_tmp_dir() {
+    let path = "./tmp";
+    let entries = read_dir(path).unwrap();
+    for entry in entries {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        if path.is_file() {
+            remove_file(path).unwrap();
+        }
+    }
 }
